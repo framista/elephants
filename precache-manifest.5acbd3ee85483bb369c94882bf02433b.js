f50b42b46ab7273442eb7c14ad2be0e1d95e90ce
framista <adajab1@gmail.com>
2020-09-28 22:18:49 +0200

    Updates

---
+++ precache-manifest.5acbd3ee85483bb369c94882bf02433b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f7d0ff5ebdc3758089192b74f8c4c4",
+    "url": "/elephants/index.html"
+  },
+  {
+    "revision": "f20d75302a16ecd2a251",
+    "url": "/elephants/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c72841328fc6f47fdad0",
+    "url": "/elephants/static/css/main.cfb8fbe8.chunk.css"
+  },
+  {
+    "revision": "f20d75302a16ecd2a251",
+    "url": "/elephants/static/js/2.ccd65291.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elephants/static/js/2.ccd65291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72841328fc6f47fdad0",
+    "url": "/elephants/static/js/main.73e5dd3d.chunk.js"
+  },
+  {
+    "revision": "3519d26877ad861380b4",
+    "url": "/elephants/static/js/runtime-main.588a547b.js"
+  }
+]);
